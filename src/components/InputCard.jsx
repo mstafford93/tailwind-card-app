28398de3dd1c782b18de8e51ba31bc9e1ef8cc4f
mstafford93 <mgstafford@hotmail.co.uk>
2026-08-28 22:17:25 +0100

    inputs refresh after submit

--- src/components/InputCard.jsx
+++ src/components/InputCard.jsx
@@ -1,17 +1,17 @@
-import React, {useState} from 'react'
+import React, { useState } from 'react'
 
-const InputCard = ({setCardData}) => {
-    const [newCard, setNewCard] = useState([{
-          title: "",
-          description: "",
-          imageUrl: ""
-        }])
+const InputCard = ({ setCardData }) => {
+    const [newCard, setNewCard] = useState({
+        title: "",
+        description: "",
+        imageUrl: ""
+    })
 
     const callNewCard = (e) => {
         const name = e.target.name;
         const value = e.target.value;
         setNewCard(ini => {
-            return {...ini, [name]: value}
+            return { ...ini, [name]: value }
         })
         console.log(newCard)
     }
@@ -21,20 +21,25 @@ const InputCard = ({setCardData}) => {
         setCardData(prevState => {
             return [...prevState, newCard]
         })
+        setNewCard({
+            title: "",
+            description: "",
+            imageUrl: ""
+        })
     }
 
-  return (
-    <div className='my-10 text-center'>
-      <input type="text" placeholder='Add Name' name='title' onChange={callNewCard} value={newCard.title}
-       className='mx-5 border-2 border-blue-600 rounded-md p-2'/>
-      <input type="text" placeholder='Add Description' name='description' onChange={callNewCard} value={newCard.description}
-      className='mx-5 border-2 border-blue-600 rounded-md p-2'/>
-      <input type="text" placeholder='Add Image URL' name='imageUrl' onChange={callNewCard} value={newCard.imageUrl}
-       className='mx-5 border-2 border-blue-600 rounded-md p-2'/>
-      <button onClick={callSubmitCard}
-      className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5">Add Card</button>
-    </div>
-  )
+    return (
+        <div className='my-10 text-center'>
+            <input type="text" placeholder='Add Name' name='title' onChange={callNewCard} value={newCard.title}
+                className='mx-5 border-2 border-blue-600 rounded-md p-2' />
+            <input type="text" placeholder='Add Description' name='description' onChange={callNewCard} value={newCard.description}
+                className='mx-5 border-2 border-blue-600 rounded-md p-2' />
+            <input type="text" placeholder='Add Image URL' name='imageUrl' onChange={callNewCard} value={newCard.imageUrl}
+                className='mx-5 border-2 border-blue-600 rounded-md p-2' />
+            <button onClick={callSubmitCard}
+                className="bg-blue-500 hover:bg-blue-700 text-white font-bold py-2 px-4 rounded mx-5">Add Card</button>
+        </div>
+    )
 }
 
 export default InputCard
